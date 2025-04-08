0017b7d1c432274a37d6221ed9bd5a91699b5ab2
KendrickNguyen76 <kendricknguyen76@gmail.com>
2025-04-08 00:34:13 -0700

    Found way to use .env to make setup easier

--- django_backend/byte_me/settings.py
+++ django_backend/byte_me/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+# Load dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,25 +81,16 @@ WSGI_APPLICATION = 'byte_me.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'HOST': 'aws-0-us-west-1.pooler.supabase.com',
-        'PORT': '6543',
-        'DATABASE': 'postgres',
-        'NAME': 'postgres',
-        'USER': 'postgres.ckcogrvcrqmgwpdrkeae',
-        'PASSWORD': # SHOULD NOT BE HARDOCDED, BUT FOR TESTING PURPOSES WE WILL DO SO
-        'POOL_MODE': 'transaction',
+        'HOST': os.environ.get('HOST'),
+        'PORT': os.environ.get('PORT'),
+        'DATABASE': os.environ.get('DATABASE'),
+        'NAME': os.environ.get('NAME'),
+        'USER': os.environ.get('USERNAME'),
+        'PASSWORD': os.environ.get('PASSWORD'),
+        'POOL_MODE': os.environ.get('POOL_MODE'),
     }
 }
 
-# Default configuration, delete later
-"""
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-"""
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
