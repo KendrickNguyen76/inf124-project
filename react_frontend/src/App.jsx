53a534c5b596ad81ac29b315055129707ced3332
soniaheyer <heyers@uci.edu>
2025-05-02 13:34:56 -0700

    added flexible footer w about us placeholder

--- react_frontend/src/App.jsx
+++ react_frontend/src/App.jsx
@@ -1,11 +1,19 @@
 import { useState } from 'react'
 import Navbar from './Components/Navbar/Navbar'
+import Footer from './Components/Navbar/Footer'
 import './App.css'
 const App = () => {
   return (
-  <div className='container'>
-    
-    <Navbar/>
-  </div>)
-}
+    <div className="page-wrapper">
+      <div className="container">
+        <Navbar />
+      </div>
+
+      <div className="bottom-container">
+        <Footer />
+      </div>
+    </div>
+  );
+};
+
 export default App
