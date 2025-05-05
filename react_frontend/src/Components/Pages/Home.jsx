4019c5344f3118f1b151b5482a909577762ccfdf
davidjoves <davidjoves20@gmail.com>
2025-05-04 21:33:40 -0700

    started home page - landing section 1 completed

--- react_frontend/src/Components/Pages/Home.jsx
+++ react_frontend/src/Components/Pages/Home.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import './Home.css';
+import Bash from "../../assets/bash.png";
+import hsaB from "../../assets/hsaB.png";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+  <div className ='home'>
+    <div className='landing_container'>
+      <div className='landing_text'>
+        <h1>Challenge. Code. Win.</h1>
+        <h2>ByteMe - The place to deal with all your coding beef.</h2>
+      </div>
+      <div className='landing_image'>
+        <img id='landing_bash' src={Bash} alt="Image of Bash - A green computer chip" />
+        <img id ='landing_hsaB' src={hsaB} alt="Image of hsaB - A blue computer chip" />
+      </div>
+    </div>
+
+    
+  </div>
+  );
 };
 
 export default Home;
