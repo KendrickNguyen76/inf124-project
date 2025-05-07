68e1eac2907e81e3ef9fca07d0060b21b518b0bf
KendrickNguyen76 <kendricknguyen76@gmail.com>
2025-05-07 12:13:55 -0700

    Got basic JSX components set up for login, need CSS now

--- react_frontend/src/Components/Pages/LoginPage.jsx
+++ react_frontend/src/Components/Pages/LoginPage.jsx
@@ -12,9 +12,15 @@ const LoginPage = ({ setLoggedIn }) => {
   return (
     <div style={{ textAlign: 'center', marginTop: '3rem' }}>
       <h2>Login Page</h2>
+
       <form action="" method="post">
-        
+          <label for="username">Username/Email</label>
+          <input type="text" id="username" name="username" placeholder="Enter Username" />
+
+          <label for="password">Password</label>
+          <input type="password" id="password" name="password" placeholder="Enter Password" />
       </form>
+
       <button onClick={handleLogin}>Log In</button>
     </div>
   );
