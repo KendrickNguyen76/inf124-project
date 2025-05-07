ba2e793edfc8b10fc8d48da2b5d2d6daf8537d94
KendrickNguyen76 <kendricknguyen76@gmail.com>
2025-05-07 15:01:09 -0700

    Finished login page, still need to figure out why it's not even with About Us

--- react_frontend/src/Components/Pages/LoginPage.jsx
+++ react_frontend/src/Components/Pages/LoginPage.jsx
@@ -3,6 +3,32 @@ import './LoginPage.css'
 import Logo from "../../assets/ByteMe_Logo.png";
 import { useNavigate } from 'react-router-dom';
 
+function InputBoxes() {
+  return (
+    <div className="inputDiv">
+      <form action="" method="post">
+        <label className="inputLabel" htmlFor="username">Username/Email</label>
+        <input className="textInput" type="text" id="username" name="username" placeholder="Enter Username" />
+
+        <label className="inputLabel" htmlFor="password">Password</label>
+        <input className="textInput" type="password" id="password" name="password" placeholder="Enter Password" />
+      </form>
+
+      <a className="forgetPassLink" href="https://www.alz.org/alzheimers-dementia/what-is-dementia">Forget Password?</a>
+    </div>
+  );
+}
+
+function Divider() {
+  return (
+    <div className="divider">
+      <div className="line"></div>
+        <label className="inputLabel">No Account?</label>
+      <div className="line"></div>
+    </div>
+  );
+}
+
 const LoginPage = ({ setLoggedIn }) => {
   const navigate = useNavigate();
 
@@ -11,23 +37,15 @@ const LoginPage = ({ setLoggedIn }) => {
     navigate('/'); // Redirect after login
   };
 
+  // Create Account button still needs code to work
+
   return (
     <div className="loginDiv">
       <img className="logo" src={Logo}/>
-
-      <div className="inputDiv">
-        <form action="" method="post">
-          <label className="inputLabel" htmlFor="username">Username/Email</label>
-          <input className="textInput" type="text" id="username" name="username" placeholder="Enter Username" />
-
-          <label className="inputLabel" htmlFor="password">Password</label>
-          <input className="textInput" type="password" id="password" name="password" placeholder="Enter Password" />
-        </form>
-
-        <a className="forgetPassLink" href="https://www.alz.org/alzheimers-dementia/what-is-dementia">Forget Password?</a>
-      </div>
-
+      <InputBoxes />
       <button className="submitButton" onClick={handleLogin}>Log In</button>
+      <Divider />
+      <button className="submitButton" id="createAccount" onClick="">Create Account</button>
     </div>
   );
 };
