6905188c49f064b239932203e3677588c299cbe8
davidjoves <davidjoves20@gmail.com>
2025-05-08 13:40:25 -0700

    added CodeEditor.jsx and started playing with monaco api and added css file for it'

---
+++ react_frontend/src/Components/CodeEditor/CodeEditor.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import './GamePage.css';
+import {Editor} from 'react-monaco-editor';
+
+const CodeEditor = () => {
+    const editorOptions = {
+        selectOnLineNumbers: true,
+        automaticLayout: true,
+        fontSize: 16,
+        lineHeight: 24,
+        minimap: {
+            enabled: false
+        },
+        scrollBeyondLastLine: false,
+        theme: 'vs-dark',
+    };
+
+    return (
+        <div className="code-editor">
+            <Editor
+                height="90vh"
+                width="100%"
+                language="javascript"
+                theme="vs-dark"
+                options={editorOptions}
+            />
+        </div>
+    );
+}
+export default CodeEditor;
