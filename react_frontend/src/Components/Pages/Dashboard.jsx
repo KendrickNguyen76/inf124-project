22bbb63221650b8b1a3ceab84eca20f5053568ca
soniaheyer <heyers@uci.edu>
2025-05-09 14:28:04 -0700

    temp dash placeholders

--- react_frontend/src/Components/Pages/Dashboard.jsx
+++ react_frontend/src/Components/Pages/Dashboard.jsx
@@ -1,7 +1,33 @@
 import React from "react";
 
+{/*title of local dashboard*/}
+const LeaderDashTitle = () => (
+  <div className = "leader_dash_title">
+    <h1> Local LeaderBoard </h1>
+  </div>
+);
+
+{/*leaderboard rank, title, etc. labels*/}
+const LeaderDashLabels = () => (
+  <div className = "leader_dash_label">
+    <div className = "label_row">
+      <span>Name</span>
+      <span>Rank</span>
+      <span>Games</span>
+    </div>
+  </div>
+)
+
+{/*put all container sections together here*/}
 const Dashboard = () => {
-  return <div>Dashboard Page</div>;
+  return(
+    <div className = "dashboard">
+      <div className = "leadercontainer">
+        <LeaderDashTitle/>
+        <LeaderDashLabels/>
+      </div>
+    </div>
+  )
 };
 
 export default Dashboard;
