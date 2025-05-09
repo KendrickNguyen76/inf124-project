1685331a7e72990ecdfc365026f89a381028222b
Baly Martinez <balym@uci.edu>
2025-05-08 20:34:27 -0700

    User Profile Page

--- react_frontend/src/Components/Pages/index.js
+++ react_frontend/src/Components/Pages/index.js
@@ -1,9 +1,9 @@
-export { default as Home } from "./Home";
-export { default as HowToPlay } from "./HowToPlay";
-export { default as LeaderBoard } from "./LeaderBoard";
-export { default as Dashboard } from "./Dashboard";
-export { default as UserProfile } from "./UserProfile";
-export { default as LoginPage } from "./LoginPage";
+export {default as Home } from "./Home";
+export {default as HowToPlay } from "./HowToPlay";
+export {default as LeaderBoard } from "./LeaderBoard";
+export {default as Dashboard } from "./Dashboard";
+export {default as UserProfile } from "./UserProfile";
+export {default as LoginPage } from "./LoginPage";
 export {default as AboutUs } from "./AboutUs";
 export {default as CreateAccount } from "./CreateAccount";
 export {default as GamePage} from "./GamePage";
