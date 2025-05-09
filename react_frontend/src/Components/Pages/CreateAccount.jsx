5ad021d4fb99216414370abccdef1cc7a4c514c8
KendrickNguyen76 <kendricknguyen76@gmail.com>
2025-05-09 13:28:40 -0700

    Finished formatting color selection for Create Account page

--- react_frontend/src/Components/Pages/CreateAccount.jsx
+++ react_frontend/src/Components/Pages/CreateAccount.jsx
@@ -1,4 +1,6 @@
 import './CreateAccount.css'
+import Bash from '../../assets/Bash.png'
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 function CreateAccountInputBoxes() {
@@ -25,9 +27,59 @@ function CreateAccountInputBoxes() {
   );
 }
 
+/*
+function BashSelection () {
+  return (
+    <div className="bashSelectionDiv">
+      <img className="selectedBash" src={Bash}/>
+
+      <form action="">
+        <input type="radio" className="colorSelectionRadio" id="green" name="colorSelection" value="Green" checked={true}/>
+        <input type="radio" className="colorSelectionRadio" id="blue" name="colorSelection" value="Blue" />
+        <input type="radio" className="colorSelectionRadio" id="red" name="colorSelection" value="Red" />
+        <input type="radio" className="colorSelectionRadio" id="orange" name="colorSelection" value="Orange" />
+        <input type="radio" className="colorSelectionRadio" id="purple" name="colorSelection" value="Purple" />
+        <input type="radio" className="colorSelectionRadio" id="pink" name="colorSelection" value="Pink" />
+      </form>
+    </div>
+  );
+}
+*/
+
+/* I ChatGPTed this and fixed it up, hopefully this works..*/
+function BashSelection() {
+  const [selectedColor, setSelectedColor] = useState('Green');
+
+  const handleColorChange = (value) => {
+    setSelectedColor(value);
+  };
+
+  return (
+    <div className="bashSelectionDiv">
+      <img className="selectedBash" src={Bash} alt="Bash" />
+
+      <form>
+        {['Green', 'Blue', 'Red', 'Orange', 'Purple', 'Pink'].map((color) => (
+            <input
+              type="radio"
+              className="colorSelectionRadio"
+              id={color.toLowerCase()}
+              name="colorSelection"
+              value={color}
+              checked={selectedColor === color}
+              onChange={() => handleColorChange(color)}
+            />
+        ))}
+      </form>
+    </div>
+  );
+}
+
 function CreateAccount () {
   return (
     <div className="createAccountDiv">
+      <h3 className="bashCustomizeHeader">Customize Your Bash!</h3>
+      <BashSelection />
       <CreateAccountInputBoxes />
       <button className="submitButton" id="createAccount">Create Account</button>
     </div>
