20e5caeba2bf68e37b73b2c6ca7cfbae8ffbb174
KendrickNguyen76 <kendricknguyen76@gmail.com>
2025-05-20 16:08:35 -0700

    Tested connection to supabase, seems like it works

--- express_backend/server.js
+++ express_backend/server.js
@@ -10,11 +10,22 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-
 app.get('/', (req, res) => {
   res.send('Hello World!')
+
+});
+
+app.get('/supabase_test', async (req, res) => {
+  const { data, error } = await supabase
+    .from('test_table')
+    .select()
+
+  res.send(data);
 });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 });
+
+
+
