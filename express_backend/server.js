53c8c279fbeb4a303c29bdf75ee3f24af2526e50
Baly Martinez <balym@uci.edu>
2025-06-01 02:12:36 -0700

    modify server.js - CORS configuration

--- express_backend/server.js
+++ express_backend/server.js
@@ -16,6 +16,11 @@ const cors = require('cors');
 const authRoutes = require('./routes/authRoutes');
 const regRoutes = require('./routes/regRoutes');
 
+// CORS configuration; setup origin
+const prodOrigin = [process.env.ORIGIN_1, process.env.ORIGIN_2].filter(Boolean);
+const devOrigin = ['http://localhost:5173'];
+const allowedOrigins = process.env.NODE_ENV === 'production' ? prodOrigin : devOrigin;
+
 app.use(cors());
 app.use(express.json()); // Add this to parse JSON bodies
 app.use('/login', authRoutes);
