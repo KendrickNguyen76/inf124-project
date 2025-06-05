c4ac029b40997a6e641419c8dc4c6d094c30711a
KendrickNguyen76 <kendricknguyen76@gmail.com>
2025-06-05 15:44:08 -0700

    Pivoting to figuring out how to track user login

--- express_backend/controllers/settingsController.js
+++ express_backend/controllers/settingsController.js
@@ -6,5 +6,9 @@ const { supabase } = require("../services/supabaseClient");
 // editProfile - handles requests/responses for the "Edit Profile"
 // part of the user settings page
 async function editProfile(req, res) {
-    // Come back to this later
+}
+
+// getProfileDetails - pulls all of the details
+// about the current user from the profile table
+async function getProfileDetails(req, res){
 }
--- react_frontend/src/Components/Pages/modules/userSettingsCrud.js
+++ react_frontend/src/Components/Pages/modules/userSettingsCrud.js
@@ -1,1 +1,7 @@
 // userSettingsCrud.js
+
+const API_URL = import.meta.env.VITE_API_URL?.replace(/\/$/, "") || "http://localhost:3000";
+
+function getCurrBash() {
+  // Come back to this
+}
