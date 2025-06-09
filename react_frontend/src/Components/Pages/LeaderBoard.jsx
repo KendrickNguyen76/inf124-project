3c8afce8203b3616ff80a2f988746c69602ddf06
Baly Martinez <balym@uci.edu>
2025-06-09 01:44:27 -0700

    modified leaderboard page

--- react_frontend/src/Components/Pages/LeaderBoard.jsx
+++ react_frontend/src/Components/Pages/LeaderBoard.jsx
@@ -2,42 +2,57 @@ import React from "react";
 import "./LeaderBoard.css";
 
 {/*title of global leaderboard*/}
-const LeaderGlobalTitle = () => (
-  <div className="leader_global_title">
-      <h1>⸺ Global LeaderBoard ⸺</h1>
+const LeaderGlobalTitle = () => ( 
+  <div className="page-title">
+  <div className="name_line"></div>
+    <h1> Global Leaderboard</h1>
+  <div className="name_line"></div>
 </div>
 );
 
-{/*leaderboard rank, title, etc. labels*/}
-const LeaderGlobalLabels = () => (
-  <div className="label_row">
-  <span>Player</span>
-  <span>Rank</span>
-  <span>Total Games</span>
-  <span>Game Ratio</span>
-</div>
-);
+// {/*leaderboard rank, title, etc. labels*/}
+// const LeaderGlobalLabels = () => (
+//   <div className="label_row">
+//     <span>Player</span>
+//     <span>Rank</span>
+//     <span>Total Games</span>
+//     <span>Game Ratio</span>
+// </div>
+// );
 
-
-{/*container for top 10 global leaderboard stats*/}
 const LeaderGlobalScrollable = () => (
-<div className="leaderboard_global_scroll">
-  {[...Array(10)].map((_, i) => (
-    <div key={i} className="data_row">
-      <span>Player {i + 1}</span>
-      <span>#{i + 1}</span>
-      <span>{Math.floor(Math.random() * 10)}</span>
-      <span>{Math.floor(Math.random() * 10)} : {Math.floor(Math.random() * 10)}</span>
+  <div className="table_container">  
+    <div className="leaderboard_global_scroll">
+      <table>
+        <thead>
+          <tr>
+            <th>Player</th>
+            <th>Rank</th>
+            <th>Total Games</th>
+            <th>Game Ratio</th>
+          </tr>
+        </thead>
+        <tbody>
+          {[...Array(10)].map((_, i) => (
+            <tr key={i}>
+              <td><span>Player {i + 1}</span></td>
+              <td><span>#{i + 1}</span></td>
+              <td><span>{Math.floor(Math.random() * 10)}</span></td>
+              <td><span>{Math.floor(Math.random() * 10)} : {Math.floor(Math.random() * 10)}</span></td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
-  ))}
-</div>
+  </div>
 );
+
 const Leaderboard = () => {
   return (
     <div className = "LeaderContainer">
       <LeaderGlobalTitle/>
-      <LeaderGlobalLabels/>
       <LeaderGlobalScrollable/>
+      
     </div>
   )
 };
